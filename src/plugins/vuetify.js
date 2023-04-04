ed04be3d77979c50ba8c5df5fb4da36bedb51a08
0521kimyi <0521kimyi@gmail.com>
2023-04-04 13:28:52 +0900

    vue add vuetify 했음 - 1차 커밋

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,10 @@
-import { createVuetify } from 'vuetify'
+// Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-import '~vuetify/dist/vuetify.min.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
-const vuetify = createVuetify({
-    theme: {
-        // add your theme settings here
-    },
-    ssr: true,
-})
+// Vuetify
+import { createVuetify } from 'vuetify'
 
-export default vuetify
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
