ad8db71725dc5f25c2b439693c3d72962d42c685
0521kimyi <0521kimyi@gmail.com>
2023-04-05 07:10:49 +0900

    index.js 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LaundryListVue from '@/views/LaundryList.vue'
+import UsageHistoryVue from '@/views/UsageHistory.vue'
+import MyPageVue from '@/views/MyPage.vue'
+import LoginFormVue from '@/views/LoginForm.vue'
 
 const routes = [
   {
@@ -8,13 +12,26 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/laundarylist',
+    name: 'laundary',
+    component: LaundryListVue
+  },
+  {
+    path: '/usagehistory',
+    name: 'usage',
+    component: UsageHistoryVue
+  },
+  {
+    path: '/mypage',
+    name: 'mypage',
+    component: MyPageVue
+  },
+
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginFormVue
+  },
 ]
 
 const router = createRouter({
