c66badf444cb2aa26b5eb5531521ffad6e318fe5
0521kimyi <0521kimyi@gmail.com>
2023-04-11 16:07:48 +0900

    **주변세탁소 컴포넌트로 laundrys.json axios**

--- src/views/AroundLaundry.vue
+++ src/views/AroundLaundry.vue
@@ -2,17 +2,17 @@
 
 <template>
     <div id="laund-list">
-        <v-card class="mx-auto my-5" max-width="500">
+        <v-card class="mx-auto my-5" max-width="500" v-for="laundry in laundrys" :key="laundry.id">
             <v-img class="align-end text-white" id="laund-img" height="150"
                 src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
-                <v-card-title id="laund-title">@@ 세탁소</v-card-title>
+                <v-card-title id="laund-title" >{{ laundry.title }}</v-card-title>
             </v-img>
 
-            <v-card-subtitle class="pt-3" id="laund-sub-title">세탁소 서브 타이틀</v-card-subtitle>
+            <v-card-subtitle class="pt-3" id="laund-sub-title">{{ laundry.subtitle }}</v-card-subtitle>
 
             <v-card-text>
-                <div id="laund-address">경기 성남시 분당구 불정로 6 그린팩토리</div>
-                <div id="laund-info">간단한 세탁소 정보</div>
+                <div id="laund-address">{{ laundry.address }}</div>
+                <div id="laund-info">{{ laundry.info }}</div>
             </v-card-text>
 
             <v-card-actions>
@@ -25,10 +25,20 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     data: () => ({
         show: false,
+        laundrys: [] // Add this line to initialize the laundry array
     }),
-
+    async created() {
+        try {
+            const res = await axios.get('http://localhost:3000/laundrys');
+            this.laundrys = res.data;
+        } catch (e) {
+            console.error(e);
+        }
+    }
 }
 </script>
