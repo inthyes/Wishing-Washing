5dd16a186c198c0a013a63a9744c7276499fe766
0521kimyi <0521kimyi@gmail.com>
2023-04-24 12:25:01 +0900

    새로 생성한 컴포넌트들 router 추가

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,15 @@ import SubmitLaundry from '@/views/SubmitLaundry.vue'
 import LoginFormVue from '@/views/LoginForm.vue'
 import SignUpFormVue from '@/views/SignUpForm.vue'
 
+import AddReviewVue from '@/views/AddReview.vue'
+import ReviewListVue from '@/views/ReviewList.vue'
+import EditProfileVue from '@/views/EditProfile.vue'
+import FavoriteLaundryVue from '@/views/FavoriteLaundry.vue'
+import QnAVue from '@/views/QnA.vue'
+import WWNoticeVue from '@/views/WWNotice.vue'
+import NoticeDetail1Vue from '@/views/NoticeDetail1.vue'
+import NoticeDetail2Vue from '@/views/NoticeDetail2.vue'
+
 const routes = [
   {
     path: '/',
@@ -33,7 +42,7 @@ const routes = [
     component: MyPageVue
   },
   {
-    path: '/laundrydetail/:id', 
+    path: '/laundrydetail/:id',
     name: 'laundrydetail',
     component: LaundryDetailVue,
     props: true
@@ -53,6 +62,46 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: SignUpFormVue
+  },
+  {
+    path: '/addreview',
+    name: 'addreview',
+    component: AddReviewVue
+  },
+  {
+    path: '/reviewlist',
+    name: 'reviewlist',
+    component: ReviewListVue
+  },
+  {
+    path: '/editprofile',
+    name: 'editprofile',
+    component: EditProfileVue
+  },
+  {
+    path: '/favoritelaundry',
+    name: 'favoritelaundry',
+    component: FavoriteLaundryVue
+  },
+  {
+    path: '/qna',
+    name: 'qna',
+    component: QnAVue
+  },
+  {
+    path: '/wwnotice',
+    name: 'wwnotice',
+    component: WWNoticeVue
+  },
+  {
+    path: '/noticedetail1',
+    name: 'noticedetail1',
+    component: NoticeDetail1Vue
+  },
+  {
+    path: '/noticedetail2',
+    name: 'noticedetail2',
+    component: NoticeDetail2Vue
   },
 ]
 
