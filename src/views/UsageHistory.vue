5798990a3d21222837cc88a28e05c98e4d0fd549
0521kimyi <0521kimyi@gmail.com>
2023-04-24 12:21:16 +0900

    이용내역 컴포넌트

--- src/views/UsageHistory.vue
+++ src/views/UsageHistory.vue
@@ -2,143 +2,135 @@
 
 <template>
     <div class="wrapper">
-        <div class="content">
+      <div class="content">
+        <v-carousel cycle height="10" hide-delimiter-background show-arrows="hover"></v-carousel>
             <div class="myWashing">
-                <h5><b>내 세탁물</b></h5>
+                <b>내 세탁물</b>
             </div>
+            <hr id="divisionLine">
 
-            <div class="date" id="date">
-                <b>2023.03.18</b>
-            </div>
-            <div class="washingStatus">
-                <span><img id="washingImg" src="./assets/knit.jpg"></span>
-                <span>
-                    <p id="name"> 품 &nbsp;&nbsp;&nbsp; 목 | 니트 &nbsp;∙<a class="delivery" style="color: red;"> 배송중</a>
-                    </p>
-                    <p id="cost">세탁비용 | 10,000원</p>
-                    <p id="requirement">요청사항 | 얼룩 깨끗하게 지워주세요</p>
-                </span>
+            <div v-for="(h, index) in historys" v-bind:key="h.id">
+                <div v-if="index === 0 || h.date !== historys[index - 1].date">
+                    <div class="date" id="date">
+                        <b>{{ h.date }}</b>
+                    </div>
+                </div>
+                <v-card v-bind:key="h.id" elevation="0">
+                    <div class="washingStatus">
+                        <v-img id="washingImg" :src="h.washingImg" cover></v-img>
+                        <v-card-text>
+                            <div id="name">
+                                품&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;목&nbsp;&nbsp;|&nbsp;&nbsp;{{ h.name }}
+                                &nbsp;&nbsp;∙&nbsp;<a class="delivery">{{ h.delivery }}</a>
+                            </div>
+                            <div id="cost">세탁비용&nbsp;|&nbsp;&nbsp;{{ h.cost }}</div>
+                            <div id="requirement">요청사항&nbsp;|&nbsp;&nbsp;{{ h.requirement }}</div>
+                            <v-divider id="divisionLine2"></v-divider>
+                            <a id="laundryName">{{ h.laundryName }}</a>&nbsp;
+                            <v-btn id="reviewBtn" to="addreview">리뷰</v-btn>
+                        </v-card-text>
+                    </div>
+                </v-card>
             </div>
             <br>
-
-            <hr id="myHistoryLine">
-            <div class="myHistory">
-                <h5><b>세탁 내역</b></h5>
-            </div>
-
-            <div class="date" id="date">
-                <b>2023.03.11</b>
-            </div>
-            <div class="washingStatus">
-                <span><img id="washingImg" src="./assets/shoes.jpg"></span>
-                <span>
-                    <p id="name"> 품 &nbsp;&nbsp;&nbsp; 목 | 운동화 &nbsp;∙ <a class="delivery">배송완료(3/14)</a></p>
-                    <p id="cost">세탁비용 | 12,000원</p>
-                    <p id="requirement">요청사항 | 변색 복구 부탁드립니다</p>
-                </span>
-            </div>
-            <div class="washingStatus">
-                <span><img id="washingImg" src="./assets/jean.jpg"></span>
-                <span>
-                    <p id="name"> 품 &nbsp;&nbsp;&nbsp; 목 | 청바지 &nbsp;∙ <a class="delivery">배송완료(3/14)</a></p>
-                    <p id="cost">세탁비용 | 13,000원</p>
-                    <p id="requirement">요청사항 | 기장 2cm 줄여주세요</p>
-                </span>
-            </div><br>
-
-            <div class="date" id="date">
-                <b>2023.03.10</b>
-            </div>
-            <div class="washingStatus">
-                <span><img id="washings" src="./assets/washings.png"></span>
-                <span>
-                    <p id="name"> 품 &nbsp;&nbsp;&nbsp; 목 | 종합 세탁물 &nbsp;∙ <a class="delivery">배송완료(3/12)</a></p>
-                    <p id="cost">세탁비용 | 10,000원</p>
-                    <p id="requirement">요청사항 | 없음</p>
-                </span>
-            </div>
-
-            <br>
-
         </div>
     </div>
 </template>
 
 <style>
-.history {
-    text-align: center;
-    justify-content: center;
-    margin-top: -2%;
-    margin-right: 2%;
-    position: fixed;
-    background-color: white;
-    top: 30px;
-    /*고정 위치 지정*/
-}
-
 .myWashing {
-    margin-left: 6%;
-    margin-top: 3%;
-    margin-bottom: 1%;
+    margin-top: 20px;
+    font-size: 18px;
+    text-align: center;
 }
 
-#myHistoryLine {
-    margin-left: 4%;
-    margin-right: 4%;
+#divisionLine {
+  margin-left: 5%;
+  margin-right: 5%;
+  margin-top: 20px;
+  margin-bottom: 10px;
 }
-
-.myHistory {
-    margin-left: 6%;
-    margin-top: 3%;
+#divisionLine2 { 
+    width: 70%;
+    margin-left: 95px;
+    margin-top: 10px;
+    margin-bottom: 5px;
 }
 
 .date {
+    margin-top: 40px;
     margin-left: 6%;
     font-size: 15px;
 }
 
 .washingStatus {
-    height: 85px;
+    height: 141px;
     border-radius: 13px;
     font-size: smaller;
     background-color: white;
     border: 1px solid gainsboro;
     font-display: center;
-    margin: 5%;
-    margin-top: 1.5%;
+    margin-left: 5%;
+    margin-right: 5%;
+    margin-bottom: 5px;
+    margin-top: 10px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: auto;
 }
 
 #name {
-    margin-bottom: 5px;
+    margin-bottom: 3px;
 }
-
 #cost {
-    margin-bottom: 5px;
+    margin-bottom: 3px;
 }
-
-#requirement {}
+#requirement {
+    margin-bottom: 3px;
+}
+#laundryName {
+    margin-left: 4px;
+}
+#reviewBtn {
+    width: 10px;
+    height: 24px;
+    background-color: gainsboro;
+    border-radius: 10px;
+    margin-top: 1px;
+    box-shadow: none;
+}
 
 .delivery {
     color: red;
+    text-decoration-line: none;
 }
 
 #washingImg {
-    width: 70px;
-    height: 70px;
+    width: 85px;
+    height: 100px;
     float: left;
-    margin-left: 3%;
-    margin-top: 1%;
-    margin-right: 3%;
-    border-radius: 15%;
-}
-
-#washings {
-    width: 60px;
-    height: 60px;
-    float: left;
-    margin-left: 3%;
-    margin-top: 4%;
-    margin-right: 3%;
-    border-radius: 15%;
+    margin-left: 15px;
+    margin-top: 19px;
+    margin-right: 17px;
+    border-radius: 5px;
 }
 </style>
+
+<script>
+import axios from "axios";
+
+export default {
+    data: () => ({
+        show: false,
+        historys: []
+    }),
+    async created() {
+        try {
+            const res = await axios.get('http://localhost:3002/historys');
+            this.historys = res.data;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
+</script>
