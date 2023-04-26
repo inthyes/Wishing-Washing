dcc07de596f1294ad03f572f704bc279896fd25e
0521kimyi <0521kimyi@gmail.com>
2023-04-26 20:19:50 +0900

    로그인 수정

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,42 +1,25 @@
 <!-- 로그인 -->
 
 <template>
-    <div class="loginform">
+    <v-card class="mx-auto pa-12" max-width="344" elevation="0">
+      <v-form v-model="form" @submit.prevent="onSubmit">
+        <v-text-field v-model="email" :readonly="loading" :rules="[required]" class="mb-2" clearable
+          label="Email"></v-text-field>
 
-       <v-container fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" md="8" lg="4">
-            <v-card class="elevation-12">
-              <v-toolbar color="white" dark>
-                <v-toolbar-title>로그인</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <v-form @submit.prevent="login">
-                  <v-text-field
-                    v-model="email"
-                    label="Email"
-                    type="email"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="password"
-                    label="Password"
-                    type="password"
-                    required
-                  ></v-text-field>
-                  <v-btn type="submit" color="blue" block>Login</v-btn>
-                </v-form>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+        <v-text-field v-model="password" :readonly="loading" :rules="[required]" clearable label="Password"
+          placeholder="Enter your password"></v-text-field>
 
-    </div>
+        <br>
+
+        <v-btn :disabled="!form" :loading="loading" block color="success" size="large" type="submit" variant="elevated">
+          로그인
+        </v-btn>
+      </v-form>
+    </v-card>
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
 
 export default {
     data() {
@@ -44,14 +27,6 @@ export default {
             email: "",
             password: "",
         };
-    },
-    methods: {
-        login() {
-            axios.post('http://localhost:3000/users', {
-        username: this.username,
-        password: this.password
-      })
-        },
     },
 };
 </script>
