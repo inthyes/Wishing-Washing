2cef645b4e199a0fd9da089fbe73db714af80b71
0521kimyi <0521kimyi@gmail.com>
2023-04-27 15:47:22 +0900

    로그인 폼 수정

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -3,11 +3,14 @@
 <template>
     <v-card class="mx-auto pa-12" max-width="344" elevation="0">
       <v-form v-model="form" @submit.prevent="onSubmit">
-        <v-text-field v-model="email" :readonly="loading" :rules="[required]" class="mb-2" clearable
-          label="Email"></v-text-field>
 
-        <v-text-field v-model="password" :readonly="loading" :rules="[required]" clearable label="Password"
-          placeholder="Enter your password"></v-text-field>
+        <v-text-field v-model="id" :readonly="loading" :rules="[required]" class="mb-2" clearable
+          label="아이디" placeholder="아이디를 입력하세요" variant="underlined"></v-text-field>
+
+        <v-text-field v-model="password" :readonly="loading" :rules="[required]" clearable 
+          label="비밀번호" placeholder="비밀번호를 입력하세요" variant="underlined" 
+           :type="show1 ? 'text' : 'password'" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                      @click:append="show1 = !show1"></v-text-field>
 
         <br>
 
@@ -24,8 +27,10 @@
 export default {
     data() {
         return {
-            email: "",
+            id: "",
             password: "",
+
+            show1: false,
         };
     },
 };
