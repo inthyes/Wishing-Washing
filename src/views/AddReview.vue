9b5673aebd3ce98bf24d99950df4a2826c3b1a8f
0521kimyi <0521kimyi@gmail.com>
2023-05-03 19:15:59 +0900

    리뷰작성 컴포넌트 수정

--- src/views/AddReview.vue
+++ src/views/AddReview.vue
@@ -7,76 +7,80 @@
             <hr id="divisionLine">
 
             <form class="add-review-form" @submit.prevent="submitReview">
-                    <div class="add-review-input-container">
-                        <label class="add-review-label" for="title">
-                            제목
-                        </label>
-                        <input class="add-review-input" type="text" id="title" v-model="review.title" required>
-                    </div>
-                    <div class="add-review-input-container">
-                        <label class="add-review-label" for="content">
-                            내용
-                        </label>
-                        <textarea class="add-review-input add-review-textarea" id="content" v-model="review.content"
-                            required></textarea>
-                    </div>
-                    <div class="add-review-input-container">
-                        <label class="add-review-label" for="rating">
-                            별점
-                        </label>
-                        <select class="add-review-input" id="rating" v-model.number="review.rating" required>
-                            <option value="" disabled>
-                                별점 선택
-                            </option>
-                            <option v-for="i in 5" :key="i" :value="i">{{ i }}점</option>
-                        </select>
+                <div class="add-review-input-container">
+                    <label class="add-review-label" for="title">
+                        제목
+                    </label>
+                    <input class="add-review-input" type="text" id="title" v-model="review.title" required>
                 </div>
-                <v-btn id="reviewBtn" to="reviewlist">
-                작성 완료    
-                </v-btn>
-                
+                <div class="add-review-input-container">
+                    <label class="add-review-label" for="content">
+                        내용
+                    </label>
+                    <textarea class="add-review-input add-review-textarea" id="content" v-model="review.content"
+                        required></textarea>
+                </div>
+                <div class="add-review-input-container">
+                    <label class="add-review-label" for="rating">
+                        별점
+                    </label>
+                    <select class="add-review-input" id="rating" v-model.number="review.rating" required>
+                        <option value="" disabled>
+                            별점 선택
+                        </option>
+                        <option v-for="i in 5" :key="i" :value="i">{{ i }}점</option>
+                    </select>
+                </div>
+                <div class="add-review-btn-container">
+                    <button class="add-review-submit-btn" type="submit">작성 완료</button>
+                </div>
             </form>
 
         </h2>
     </div>
 </template>
 
-
 <script>
 import axios from "axios";
 export default {
-  data() {
-    return {
-      show: false,
-      historys: [],
-      review: {
-        title: '',
-        content: '',
-        rating: 0 // rating 속성을 0으로 초기화
-      }
-    };
-  },
-  async created() {
-    try {
-      const res = await axios.get('http://localhost:3002/historys');
-      this.historys = res.data;
-    } catch (e) {
-      console.error(e);
+    data() {
+        return {
+            review: {
+                title: '',
+                content: '',
+                rating: 0
+            }
+        };
+    },
+    methods: {
+        submitReview() {
+            // 현재 날짜 및 시간 가져오기
+            const date = new Date();
+            const dateString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+            // 새로운 리뷰 데이터 생성
+            const newReview = {
+                title: this.review.title,
+                content: this.review.content,
+                rating: this.review.rating,
+                date: dateString
+            };
+            // reviews.json 파일에 새로운 리뷰 데이터 추가
+            axios.post('http://localhost:3004/reviews', newReview)
+                .then(() => {
+                    // 리뷰 작성 후 폼 초기화
+                    this.review.title = '';
+                    this.review.content = '';
+                    this.review.rating = 0;
+                    // reviewlist.vue로 이동
+                    this.$router.push('/reviewlist');
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
     }
-  },
-  methods: {
-    submitReview() {
-      console.log(this.review);
-      // 리뷰 작성 후 폼 초기화
-      this.review.title = '';
-      this.review.content = '';
-      this.review.rating = 0;
-    }
-  }
 };
 </script>
-
-
 
 <style scoped>
 .add-review-container {
@@ -140,5 +144,4 @@ export default {
 
 .add-review-submit-btn:hover {
     background-color: #222;
-}
-</style>  
+}</style>
