44250ef1761f3ef3859eff7ced5d502fb6f4fc2c
임채연 <dlacodus0407@gmail.com>
2023-05-13 21:38:56 +0900

    addCart 함수 안 쓰는 변수 삭제, 변수 query > queryAddOrderList 변수명 변경

--- back/app/src/models/Cart.js
+++ back/app/src/models/Cart.js
@@ -15,69 +15,67 @@ class Cart {
         const response = await Cart.addCart(this.body, this.userID);
         return response;
     }
-      static async addCart(Info, userID) {
-            return new Promise((resolve, reject) => {
-              db.query("USE CAPSTONE", (err, result) => {
-                if (err) {
-                  reject(err);
-                  return;
-                }
-                let queryAddCart = "INSERT INTO cart(O_NUM, PRODUCT_ID, PRODUCT_QUANTITIY) VALUES ?";
-                let query = "INSERT INTO ORDER_LIST(O_NUM, S_ID, U_ID) VALUES (? , ? , ?);";
-                let queryGetOrderNum = "SELECT * FROM ORDER_NUM;";
-                let queryUpdateOrderNum = "UPDATE ORDER_NUM SET ORDER_NUM = ? WHERE ORDER_NUM = ?;";
-                let orderNum;
-                let values = [];
-                const keys = Object.keys(Info); //객체의 배열화
-                db.query(
-                  queryGetOrderNum,
-                  (err, result) => {
-                    if (err) reject(err);
-                    else  
-                    {
-                      orderNum = result[0].ORDER_NUM + 1;
-                      db.query(
-                        query,
-                        [orderNum, parseInt(Info.laundryId), userID],
-                        (err, result) => {
-                          if (err) reject(err);
-                          else {
-                            let values = [];
-                            const keys = Object.keys(Info); //객체의 배열화
-                            for (let i = 0; i < keys.length-1; i++) {
-                              const cartNum = parseInt(Info[keys[i]]); //Info.p1
-                              values.push([orderNum, keys[i], cartNum]);
-                            }   
-                            // add cart 구현 -> CART DB에 주문번호와 함꼐 INSERT
-                            db.query(
-                              queryAddCart,
-                              [values],
-                              (err, result) => {
-                                if (err) reject(err);
-                                else {                       
-                                  // ORDER_NUM TABLE에 있는 ORDER_NUM 하나 증가해서 UPDATE
-                                  db.query(
-                                    queryUpdateOrderNum,
-                                    [orderNum, orderNum-1], 
-                                    (err, data) => {
-                                    if (err) reject(err);
-                                    else {
-                                      resolve({ orderNumber: orderNum }); 
-                                        }
-                                    });              
-                                }
-                              }
-                            )
-                          }
-                        }
-                      );
-                    }
-                  }
-                );
+    static async addCart(Info, userID) {
+          return new Promise((resolve, reject) => {
+            db.query("USE CAPSTONE", (err, result) => {
+              if (err) {
+                reject(err);
+                return;
               }
-              )
+              let queryAddCart = "INSERT INTO cart(O_NUM, PRODUCT_ID, PRODUCT_QUANTITIY) VALUES ?";
+              let queryAddOrderList = "INSERT INTO ORDER_LIST(O_NUM, S_ID, U_ID) VALUES (? , ? , ?);";
+              let queryGetOrderNum = "SELECT * FROM ORDER_NUM;";
+              let queryUpdateOrderNum = "UPDATE ORDER_NUM SET ORDER_NUM = ? WHERE ORDER_NUM = ?;";
+              let orderNum;
+              db.query(
+                queryGetOrderNum,
+                (err, result) => {
+                  if (err) reject(err);
+                  else  
+                  {
+                    orderNum = result[0].ORDER_NUM + 1;
+                    db.query(
+                      queryAddOrderList,
+                      [orderNum, parseInt(Info.laundryId), userID],
+                      (err, result) => {
+                        if (err) reject(err);
+                        else {
+                          let values = [];
+                          const keys = Object.keys(Info); //객체의 배열화
+                          for (let i = 0; i < keys.length-1; i++) {
+                            const cartNum = parseInt(Info[keys[i]]); //Info.p1
+                            values.push([orderNum, keys[i], cartNum]);
+                          }   
+                          // add cart 구현 -> CART DB에 주문번호와 함꼐 INSERT
+                          db.query(
+                            queryAddCart,
+                            [values],
+                            (err, result) => {
+                              if (err) reject(err);
+                              else {                       
+                                // ORDER_NUM TABLE에 있는 ORDER_NUM 하나 증가해서 UPDATE
+                                db.query(
+                                  queryUpdateOrderNum,
+                                  [orderNum, orderNum-1], 
+                                  (err, data) => {
+                                  if (err) reject(err);
+                                  else {
+                                    resolve({ orderNumber: orderNum }); 
+                                      }
+                                  });              
+                              }
+                            }
+                          )
+                        }
+                      }
+                    );
+                  }
+                }
+              );
             }
             )
+          }
+          )
         }
 
         async deleteCart() {
