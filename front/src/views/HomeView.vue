985638f541004fecda0ce1c24db0211ce850b6c3
sxhxrx <shr2001@naver.com>
2023-05-14 19:00:46 +0900

    홈

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,25 +1,17 @@
 <!-- 사용자 홈 -->
+<!--서혜린-->
 
 <template>
   <v-carousel cycle height="250" hide-delimiter-background show-arrows="hover">
-    <v-carousel-item v-for="(slide, i) in slides" :key="i">
-      <v-sheet :color="colors[i]" height="100%">
-        <div class="d-flex fill-height justify-center align-center">
-          <div class="text-h2">
-            {{ slide }} Slide
-          </div>
-        </div>
+    <v-carousel-item v-for="slide in slides" :key="slide.id">
+      <v-sheet height="100%">
+        <v-img :src="slide.imageSrc" :alt="slide.altText" height="100%" contain></v-img>
       </v-sheet>
     </v-carousel-item>
   </v-carousel>
 
   <div class="wrapper">
     <div class="content">
-      <!--주소-->
-      <div class="address">
-        <span>경기 성남시 분당구 불정로 6 그린팩토리</span>
-      </div>
-
       <!--추천 세탁소-->
       <div class="recommend">
         <p style="font-size: 18px;"><b>이 세탁소는 어떠세요 ?</b></p>
@@ -27,6 +19,7 @@
         <div class="popularLaundry">
           <p style="font-size: 15px; padding: 8%; padding-top: 6%;"><b>우리 동네 인기 세탁소</b></p>
           <p style="font-size: 6px; padding-left: 10%; margin-top: -11%; margin-bottom: -1%;">주변에서 인기 있는 세탁소예요!</p>
+          <v-img src="@/assets/images/map.png" style="width: 260px; margin-left: 44px; margin-bottom: 25px; border-radius: 5px;"></v-img>
         </div>
       </div>
 
@@ -38,9 +31,9 @@
           <p style="font-size: 15px; padding: 8%; padding-top: 6%;"><b>드라이클리닝</b></p>
           <p style="font-size: 3px; padding-left: 10%; margin-top: -11%;">
             손 많이 가는 드라이클리닝 의류, <br>&nbsp; 집에서 어떻게 관리해야 할까 ?</p>
+            <v-img src="@/assets/images/drycleaning.png" style="width: 180px; margin-left: 170px; margin-bottom: 100px; margin-top: -20px;"></v-img>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -49,19 +42,27 @@
 export default {
   data() {
     return {
-      colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4',
-      ],
       slides: [
-        'First',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth',
+        {
+          imageSrc: require('@/assets/images/001.jpg'),
+          altText: 'First Slide',
+        },
+        {
+          imageSrc: require('@/assets/images/002.jpg'),
+          altText: 'Second Slide',
+        },
+        {
+          imageSrc: require('@/assets/images/003.jpg'),
+          altText: 'Third Slide',
+        },
+        {
+          imageSrc: require('@/assets/images/004.jpg'),
+          altText: 'Fourth Slide',
+        },
+        {
+          imageSrc: require('@/assets/images/005.jpg'),
+          altText: 'Fifth Slide',
+        },
       ],
     }
   },
@@ -69,43 +70,21 @@ export default {
 </script>
 
 <style>
-.address {
-  height: 30px;
-  background-color: rgba(228, 227, 227, 0.728);
-  border-radius: 20px;
-  align-items: center;
-  text-align: center;
-  justify-content: center;
-  margin-left: 4%;
-  margin-right: 4%;
-  margin-top: 7%;
-  margin-bottom: 7%;
-  font-size: 10px;
-  padding-bottom: 3%;
-}
-
-
-
-
 .recommend {
   margin: 5.2%;
 }
-
 .popularLaundry {
   height: 170px;
   background-color: rgba(228, 227, 227, 0.728);
   border-radius: 20px;
 }
-
 .washingTip {
   margin: 5.2%;
   margin-top: 7%;
 }
-
 .dryCleaning {
   height: 170px;
   background-color: rgba(228, 227, 227, 0.728);
   border-radius: 20px;
 }
 </style>
-
