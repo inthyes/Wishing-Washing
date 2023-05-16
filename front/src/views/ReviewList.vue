8c0578d83d2354c4210765b7abebdb0edbc3011e
0521kimyi <0521kimyi@gmail.com>
2023-05-17 00:12:03 +0900

    리뷰리스트 UI 변경

--- front/src/views/ReviewList.vue
+++ front/src/views/ReviewList.vue
@@ -1,26 +1,39 @@
 <!-- 리뷰리스트 --> <!--내가 쓴 리뷰들-->
 
 <template>
-    <div>
-        <br>
-        <h2 class="text-center">내가 쓴 리뷰</h2>
-        <hr id="div_line" />
-
-        <v-container fluid>
-            <v-row>
-                <v-col v-for="r in reviews" :key="r.O_NUM" cols="12" md="6" lg="4">
-                    <v-card>
-                        <v-card-actions>{{ r.REVIEW_STAR }}점</v-card-actions>
-                        <v-card-title>{{ r.REVIEW_TITLE }}</v-card-title>
-                        <v-card-text>{{ r.REVIEW_TEXT }}</v-card-text>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
+    <div id="review-list" class="mt-10 px-5">
+        <h5>{{ userId }}님이 작성한 리뷰 </h5>
+        <v-card v-for="r in reviews" :key="r.O_NUM" class="mx-auto mt-4" max-width="400" elevation="0">
+            <!-- 세탁소정보 / 별점 / 날짜 -->
+            <v-card-body>
+                <p class="card-text my-0">세탁소이름대신아이디 {{ r.S_ID }}></p>
+                <v-row class="mx-0 my-0">
+                    <v-rating :model-value=r.REVIEW_STAR color="amber" density="compact" half-increments readonly
+                        size="small">{{ r.REVIEW_STAR }}</v-rating>
+                    <p class="text-grey ms-2">{{ r.REGI_DATE.slice(0, 10) }} / {{ r.REGI_DATE.slice(11, 19) }} / {{ getTimeAgo(r.REGI_DATE) }}</p>
+                </v-row>
+            </v-card-body>
+            <!-- 리뷰 사진 -->
+            <img src="https://t3.ftcdn.net/jpg/03/86/68/44/240_F_386684471_AdlSqWEYArE3ekokUWeFKWb4yPrfw4Rw.jpg"
+                class="card-img-top" alt="Review Image" style="object-fit: cover; height: 200px;">
+            <!-- 리뷰제목 / 리뷰내용 -->
+            <v-card-body>
+                <h5 class="card-title mt-3">{{ r.REVIEW_TITLE }}</h5>
+                <p class="card-text mt-2">{{ r.REVIEW_TEXT }}</p>
+            </v-card-body>
+            <v-card-actions  class="mx-0 px-0 pt-5">
+                <!-- 작성한 사용자만 수정 삭제 가능 -->
+                <v-btn  variant="flat" class="custom-btn flex-grow-1" color="light-blue-darken-4"  v-if="isEditable(r)" >
+                    수정
+                </v-btn>
+                <v-btn  variant="outlined" class="custom-btn flex-grow-1" color="light-blue-darken-4" v-if="isEditable(r)">
+                    삭제
+                </v-btn>
+            </v-card-actions>
+            <v-divider class="py-0"></v-divider>
+        </v-card>
     </div>
 </template>
-
-
 
 <script>
 import axios from "axios";
@@ -36,6 +49,51 @@ export default {
             this.reviews = res.data;
         } catch (e) {
             console.error(e);
+        }
+    },
+    methods: {
+        getTimeAgo(dateString) {
+            const date = new Date(dateString);
+            const now = new Date();
+            const diffInMilliseconds = now - date;
+
+            // Calculate the difference in minutes, hours, and days
+            const minutesAgo = Math.floor(diffInMilliseconds / (1000 * 60));
+            const hoursAgo = Math.floor(diffInMilliseconds / (1000 * 60 * 60));
+            const daysAgo = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+
+            // Check if the date is after a week
+            const nextWeek = new Date(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+
+            if (diffInMilliseconds < 0) {
+                // Date is in the future
+                return nextWeek.toLocaleDateString();
+            } else if (minutesAgo < 60) {
+                // Less than an hour ago
+                return `${minutesAgo}분 전`;
+            } else if (hoursAgo < 24) {
+                // Less than a day ago
+                return `${hoursAgo}시간 전`;
+            } else if (daysAgo < 7) {
+                // Less than a week ago
+                return `${daysAgo}일 전`;
+            } else {
+                // After a week
+                return nextWeek.toLocaleDateString();
+            }
+        }
+    },
+    mounted() {
+        this.userId = localStorage.getItem("userId");
+    },
+    computed: {
+        isEditable() {
+            return (r) => {
+                if (!this.userId) {
+                    return false; // User is not logged in
+                }
+                return r.U_ID === this.userId; // Compare user IDs
+            };
         }
     }
 }
