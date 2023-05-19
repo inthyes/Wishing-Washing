9a198a1bf896f190920b3d9d9bf0ff90cec4883d
0521kimyi <0521kimyi@gmail.com>
2023-05-19 11:09:40 +0900

    store vuex

---
+++ front/src/store/modules/address.js
@@ -0,0 +1,12 @@
+// address2 값을 관리
+
+// export default {
+//     state: {
+//         address2: '',
+//     },
+//     mutations: {
+//         setAddress2(state, address) {
+//             state.address2 = address;
+//         },
+//     },
+// };
