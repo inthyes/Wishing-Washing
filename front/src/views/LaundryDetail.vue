e6dd41b6140a1b864b1fddf85c73833e47974c3f
0521kimyi <0521kimyi@gmail.com>
2023-05-20 15:31:46 +0900

    리뷰 UI 수정 (버튼 이벤트 수정해야합니다)

--- front/src/views/LaundryDetail.vue
+++ front/src/views/LaundryDetail.vue
@@ -27,7 +27,7 @@
                         size="normal"></v-rating>
 
                     <div class="text-grey ms-2">
-                        {{ reviewStar }} ({{countReview}})
+                        {{ reviewStar }} ({{ countReview }})
                     </div>
                 </v-row>
 
@@ -74,21 +74,36 @@
                     </v-list>
                 </v-window-item>
                 <v-window-item value="tab-2">
-                    <div class="text-black mx-6 mt-2">
-                     <v-container fluid>
-            <v-row>
-                <v-col v-for="r in review" :key="r.O_NUM" cols="12" md="6" lg="4">
-  <v-card>
-    <v-img v-if="r.imageUrl" :src="r.imageUrl" width="100%" height="auto"></v-img>
-    <v-card-actions>{{ r.REVIEW_STAR }}점</v-card-actions>
-    <v-card-title>{{ r.REVIEW_TITLE }}</v-card-title>
-    <v-card-text>{{ r.REVIEW_TEXT }}</v-card-text>
-  </v-card>
-
-                </v-col>
-            </v-row>
-        </v-container>
-                      
+                    <div class="mx-6 mt-2">
+                        <v-container fluid>
+                            <v-row>
+                                <v-col v-for="r in review" :key="r.O_NUM">
+                                    <div class="review-card">
+                                        <p class="text-black-grey m-0">{{ r.U_ID }}</p>
+                                        <v-row class="mx-0 my-0 mb-4">
+                                            <v-rating :model-value=r.REVIEW_STAR color="amber" density="compact"
+                                                half-increments readonly size="small">{{ r.REVIEW_STAR }}</v-rating>
+                                            <p class="text-grey ms-2 mb-0">
+                                                {{ r.REGI_DATE.slice(0, 10) }} ({{getTimeAgo(r.REGI_DATE) }})</p>
+                                        </v-row>
+                                        <v-img :width="200" cover v-if="r.imageUrl" :src="r.imageUrl" class="mb-3" ></v-img>
+                                        <p class="mb-0">{{ r.REVIEW_TEXT }}</p>
+                                        <v-card-actions class="mx-0 px-0 pt-5">
+                                            <!-- 작성한 사용자만 수정 삭제 가능 -->
+                                            <v-btn variant="flat" class="custom-btn flex-grow-1"
+                                                color="light-blue-darken-3">
+                                                수정
+                                            </v-btn>
+                                            <v-btn variant="outlined" class="custom-btn flex-grow-1"
+                                                color="light-blue-darken-3">
+                                                삭제
+                                            </v-btn>
+                                        </v-card-actions>
+                                        <v-divider :thickness="2"></v-divider>
+                                    </div>
+                                </v-col>
+                            </v-row>
+                        </v-container>
                     </div>
                 </v-window-item>
             </v-window>
@@ -100,12 +115,10 @@
                 <v-btn icon @click="toggleWish(likeStatus)">
                     <v-icon :color="likeStatus ? 'red' : ''">{{ likeStatus ? 'mdi-heart' : 'mdi-heart-outline' }}</v-icon>
                 </v-btn>
-                <v-btn class="flex-grow-1 mr-3" color="light-blue-darken-4" size="x-large" variant="tonal" @click="submitData">
+                <v-btn class="flex-grow-1 mr-3" color="light-blue-darken-4" size="x-large" variant="tonal"
+                    @click="submitData">
                     세탁신청
                 </v-btn>
-                <!-- <router-link :to="{ name: 'submitlaundry', params: { id: laundry.id } }">
-                <v-btn color="deep-purple-lighten-2">신청하기</v-btn>
-                </router-link> -->
             </v-card-actions>
         </v-card>
     </div>
@@ -115,13 +128,13 @@
 import axios from 'axios';
 
 function arrayBufferToBase64(buffer) {
-  let binary = '';
-  const bytes = new Uint8Array(buffer);
-  const len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return window.btoa(binary);
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
 }
 
 export default {
@@ -136,18 +149,18 @@ export default {
 
         reviewStar: {},
         likeStatus: {},
-        countReview : {},
-         imageUrl: "",
+        countReview: {},
+        imageUrl: "",
 
         tab: 'Appetizers',  // 세탁/수선 & 리뷰 탭
         isWished: false,    // 찜버튼
 
     }),
     async created() {
-        
+
         try {
             await this.getImageUrl(); // 이미지 URL 가져오기
-            
+
             const id = this.$route.params.id;
             const res = await axios.get(`http://localhost:3000/laundry/detail/${id}`, {
                 withCredentials: true,
@@ -163,9 +176,9 @@ export default {
             this.countReview = res.data.countReview;
 
             this.review.forEach((r, index) => {
-            r.imageUrl = this.reviewImages[index];
-            
-        });
+                r.imageUrl = this.reviewImages[index];
+
+            });
 
         } catch (e) {
             console.error(e);
@@ -174,28 +187,56 @@ export default {
     methods: {
 
         async getImageUrl() {
-        try {
-            const res = await axios.get(`http://localhost:3000/upload/laundryReview/${this.$route.params.id}`);
-            console.log(res);
+            try {
+                const res = await axios.get(`http://localhost:3000/upload/laundryReview/${this.$route.params.id}`);
+                console.log(res);
 
-            this.reviewImages = res.data.map(item => {
-            if (item.review_img) {
-                
-                const base64 = arrayBufferToBase64(item.review_img.data);
-                
-                return `data:image/png;base64,${base64}`;
+                this.reviewImages = res.data.map(item => {
+                    if (item.review_img) {
+
+                        const base64 = arrayBufferToBase64(item.review_img.data);
+
+                        return `data:image/png;base64,${base64}`;
+                    }
+                    return null;
+                });
+
+
+            } catch (error) {
+                console.error(error);
             }
-            return null;
-            });
-
-        
-        } catch (error) {
-            console.error(error);
-        }
         },
 
+        getTimeAgo(dateString) {
+            const date = new Date(dateString);
+            const now = new Date();
+            const diffInMilliseconds = now - date;
 
+            // Calculate the difference in minutes, hours, and days
+            const minutesAgo = Math.floor(diffInMilliseconds / (1000 * 60));
+            const hoursAgo = Math.floor(diffInMilliseconds / (1000 * 60 * 60));
+            const daysAgo = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
 
+            // Check if the date is after a week
+            const nextWeek = new Date(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+
+            if (diffInMilliseconds < 0) {
+                // Date is in the future
+                return nextWeek.toLocaleDateString();
+            } else if (minutesAgo < 60) {
+                // Less than an hour ago
+                return `${minutesAgo}분 전`;
+            } else if (hoursAgo < 24) {
+                // Less than a day ago
+                return `${hoursAgo}시간 전`;
+            } else if (daysAgo < 7) {
+                // Less than a week ago
+                return `${daysAgo}일 전`;
+            } else {
+                // After a week
+                return nextWeek.toLocaleDateString();
+            }
+        },
         // reserve() {
         //     this.loading = true
 
@@ -250,5 +291,5 @@ export default {
             }
         }
     }
-   }
+}
 </script>
