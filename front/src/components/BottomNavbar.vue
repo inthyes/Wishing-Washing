a37bd3fa8a826c6c256f2aa52d0ef101185afb39
s2eon <128551881+s2eon@users.noreply.github.com>
2023-05-22 02:41:10 +0900

    Update BottomNavbar.vue

--- front/src/components/BottomNavbar.vue
+++ front/src/components/BottomNavbar.vue
@@ -1,8 +1,8 @@
 <template>
-    <v-bottom-navigation v-model="value" color="teal" mode="shift">
+    <v-bottom-navigation v-model="value" height="70" color="light-blue-darken-4" mode="shift" grow>
         <v-btn router-link to="/">
             <v-icon>mdi-home</v-icon>
-            <span>HOME</span>
+            <span>&nbsp;&nbsp;HOME&nbsp;&nbsp;</span>
         </v-btn>
 
         <v-btn to="managemylaundry">
@@ -15,9 +15,9 @@
             <span>주문관리</span>
         </v-btn>
 
-        <v-btn to="managermypage2">
+        <v-btn to="managermypage">
             <v-icon>mdi-account-outline</v-icon>
-            <span>MY</span>
+            <span>&nbsp;&nbsp;&nbsp;MY&nbsp;&nbsp;&nbsp;</span>
         </v-btn>
     </v-bottom-navigation>
 </template>
@@ -27,3 +27,14 @@ export default {
     data: () => ({ value: null }),
 }
 </script>
+
+<style scoped>
+.bt-navbar {
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+}
+.nav-btn {
+    width: (100% / 4);
+    padding-inline: 0;
+}
+</style>
