b6ab97703371ef6f335a14e9f3a6d7eb81a49e0f
0521kimyi <0521kimyi@gmail.com>
2023-05-22 16:39:17 +0900

    상단앱바를 페이지구성요소 컴포넌트로 변경

---
+++ front/src/components/AppBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- 상단 Appbar -->
+  <v-app-bar :elevation="1">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer">
+        </v-app-bar-nav-icon>
+      </template>
+      <v-app-bar-title>WW</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <template v-slot:append>
+        <v-btn icon="mdi-magnify"></v-btn>
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" location="left" temporary>
+      <v-list>
+        <v-list-item router-link to="/login"> <!-- Add router link to Login -->
+          <v-list-item-title>로그인</v-list-item-title>
+        </v-list-item>
+        <v-list-item router-link to="/signup"> <!-- Add router link to Sign Up -->
+          <v-list-item-title>회원가입</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+}
+</script>
